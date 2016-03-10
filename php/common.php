01e83a9d6f40d3008fa88cf2af16c224db0f9761
Fabio <fabiocl93@gmail.com>
2016-03-10 20:16:54 +0000

    Database E Nova Componente

--- php/common.php
+++ php/common.php
@@ -1,8 +1,49 @@
 <?php
+
+//This class will handle all the operations related to the database
+public class Db_Op ()
+{
+    //DB_HOST,DB_USER,DB_PASSWORD,DB_NAME these are contants present in the wordpress
+    $mysqli;
+    //This method will make the database connection
+    public function connectToDb()
+    {
+      $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+      if($mysqli->connect_errno)
+      {
+        
+      }
+    }
+
+    public function disconnectToDb()
+    {
+      $mysqli->close();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     //Array para a componente pesquisa dinâmica.
- 
+
    function operadores()
-   {  
+   {
         $operadores = array(
             "menor"=>"<",
             "maior"=>">",
@@ -23,7 +64,7 @@
 
 	function getEnumValues($table, $field, $link) {
     $enum_array = array();
-    
+
     $query = 'SHOW COLUMNS FROM `' . $table . '` LIKE "' . $field . '"';
     $result = mysqli_query($link,$query);
     if(!$result)
@@ -33,10 +74,10 @@
     else
     {
      $row = mysqli_fetch_row($result);
-     //Trata a coluna onde está o enum e guarda o valor em enum_arry     
+     //Trata a coluna onde está o enum e guarda o valor em enum_arry
      preg_match_all("/'(.*?)'/", $row[1], $enum_array);
 
-     if(!empty($enum_array[1])) 
+     if(!empty($enum_array[1]))
      {
          // Shift array keys to match original enumerated index in MySQL (allows for use of index values instead of strings)
 
@@ -50,7 +91,7 @@
      else
      {
       return array(); // Return an empty array to avoid possible errors/warnings if array is passed to foreach() without first being checked with !empty().
-     } 
+     }
     }
 }
 ?>
