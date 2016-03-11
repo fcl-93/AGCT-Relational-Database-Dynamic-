4e86d4fe711c24914ad50384b1d96310f2e84159
vmcbaptista <2054313@student.uma.pt>
2016-03-11 17:49:25 +0000

    Update propriedades

--- php/gestao-de-propriedades.php
+++ php/gestao-de-propriedades.php
@@ -1,172 +1,180 @@
 <?php
 require_once("custom/php/common.php");
 
-$db = new Db_Op();
-
-$capability = "manage_properties";
-
-//Verifica se algum utilizador está com sessão iniciada
-if ( is_user_logged_in() )
+class PropertyManage
 {
-    // Verifica se o utilziador atual tem a capability necessária para esta componente
-    if(current_user_can($capability))
-    {
-        verificaEstado();
+    private $db;
+    private $capability;
+   
+    public function _constructor($param) {
+        $db = new Db_Op();
+        $capability = "manage_properties";
     }
-    else
+    
+    public function executaScript()
     {
-?>
+        //Verifica se algum utilizador está com sessão iniciada
+        if ( is_user_logged_in() )
+        {
+            // Verifica se o utilziador atual tem a capability necessária para esta componente
+            if(current_user_can($capability))
+            {
+                verificaEstado();
+            }
+            else
+            {
+        ?>
+                <html>
+                    <p>Não tem autorização para aceder a esta página</p>
+                </html>
+        <?php
+            }
+        }
+        else
+        {
+        ?>
+            <html>
+                <p>Não tem sessão iniciada.</p>
+                <p>Clique <a href="/login">aqui</a> para iniciar sessão.</p>
+            </html>
+        <?php
+        }
+    }
+    
+    private function verificaEstado()
+    {
+        print_r($_REQUEST);
+        if (empty($_REQUEST["estado"]))
+        {
+    ?>
         <html>
-            <p>Não tem autorização para aceder a esta página</p>
+            <p>Por favor escolha que tipo de propriedades quer gerir.</p>
+            <ol>
+                <li><a href="/gestao-de-propriedades?estado=relation">Relação</a></li>
+                <li><a href="/gestao-de-propriedades?estado=entity">Entidade</a></li>
+            </ol>
         </html>
-<?php
+    <?php
+        }
+        elseif ($_REQUEST["estado"] === "relation")
+        {
+            estadoEntityRelation("relation");
+        }
+        elseif ($_REQUEST["estado"] === "entity")
+        {
+            estadoEntityRelation("entity");
+        }
+        elseif ($_REQUEST["estado"] === "inserir")
+        {
+            estadoInserir();
+        }
     }
-}
-else
-{
-?>
-    <html>
-        <p>Não tem sessão iniciada.</p>
-        <p>Clique <a href="/login">aqui</a> para iniciar sessão.</p>
-    </html>
-<?php
-}
 
-function verificaEstado()
-{
-    print_r($_REQUEST);
-    if (empty($_REQUEST["estado"]))
+    private function existePropriedade($tipo)
     {
-?>
-    <html>
-        <p>Por favor escolha que tipo de propriedades quer gerir.</p>
-        <ol>
-            <li><a href="/gestao-de-propriedades?estado=relation">Relação</a></li>
-            <li><a href="/gestao-de-propriedades?estado=entity">Entidade</a></li>
-        </ol>
-    </html>
-<?php
-    }
-    elseif ($_REQUEST["estado"] === "relation")
-    {
-        estadoEntityRelation("relation");
-    }
-    elseif ($_REQUEST["estado"] === "entity")
-    {
-        estadoEntityRelation("entity");
-    }
-    elseif ($_REQUEST["estado"] === "inserir")
-    {
-        estadoInserir();
-    }
-}
+        echo "entrei aqui";
+        $querySelect = "SELECT * FROM PROPERTY WHERE ";
+        if ($tipo === "relation")
+        {
+            $querySelect.= "ent_type_id = NULL";
+        }
+        else
+        {
+            $querySelect.= "rel_type_id = NULL";
+        }
 
-function existePropriedade($tipo)
-{
-    echo "entrei aqui";
-    $querySelect = "SELECT * FROM PROPERTY WHERE ";
-    if ($tipo === "relation")
-    {
-        $querySelect.= "ent_type_id = NULL";
+        $reusltSelect = $db->runQuery($querySelect);
+
+        if ($resultSelect->num_rows == 0)
+        {
+    ?>
+            <html>
+                <p>Não existem propiedades especificadas para o tipo selecionado</p>
+            </html>    
+    <?php
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
     }
-    else
+
+    private function estadoEntityRelation($tipo)
     {
-        $querySelect.= "rel_type_id = NULL";
+        if(existePropriedade($tipo))
+        {
+            apresentaTabelaForm($tipo);
+        }
     }
-    
-    $reusltSelect = $db->runQuery($querySelect);
-    
-    if ($resultSelect->num_rows == 0)
+
+    private function apresentaTabelaForm($tipo)
     {
-?>
+        if ($tipo === entity)
+        {
+    ?>
         <html>
-            <p>Não existem propiedades especificadas para o tipo selecionado</p>
-        </html>    
-<?php
-        return false;
-    }
-    else
-    {
-        return true;
-    }
-    
-}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Entidade</th>
+                        <th>ID</th>
+                        <th>Propriedade</th>
+                        <th>Tipo de valor</th>
+                        <th>Nome do campo no formulário</th>
+                        <th>Tipo do campo no formulário</th>
+                        <th>Tipo de unidade</th>
+                        <th>Ordem do campo no formulário</th>
+                        <th>Tamanho do campo no formulário</th>
+                        <th>Obrigatório</th>
+                        <th>Estado</th>
+                        <th>Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <?php
+                            $selecionaProp = "SELECT * FROM property WHERE rel_type_id = NULL";
+                            $resultSeleciona = $db->runQuery($selecionaProp);
+                            $numLinhas = $resultSeleciona->num_rows();
+                            while($resultSeleciona->fetch_assoc())
+                            {
+                                $ent_type_id = $resultSeleciona["ent_type_id"];
+                                $idProp = $resultSeleciona["id"];
+                                $nome = $resultSeleciona["ent_type_id"];
+                                $value_type = $resultSeleciona["value_type"];
+                                $form_field_name = $resultSeleciona["form_field_name"];
+                                $form_field_type = $resultSeleciona["form_field_type"];
+                                $unit_type_id = $resultSeleciona["unit_type_id"];
+                                $form_field_order = $resultSeleciona["form_field_order"];
+                                $form_field_size = $resultSeleciona["form_field_size"];
+                                $mandatory = $resultSeleciona["mandatory"];
+                                $state = $resultSeleciona["state"];                            
+                            }
+                        ?>
+                        <td rowspan="<?php echo $numLinhas?>"></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </html>
+    <?php
+        }
+        else
+        {
 
-function estadoEntityRelation($tipo)
-{
-    if(existePropriedade($tipo))
-    {
-        apresentaTabelaForm($tipo);
-    }
-}
-
-function apresentaTabelaForm($tipo)
-{
-    if ($tipo === entity)
-    {
-?>
-    <html>
-        <table>
-            <thead>
-                <tr>
-                    <th>Entidade</th>
-                    <th>ID</th>
-                    <th>Propriedade</th>
-                    <th>Tipo de valor</th>
-                    <th>Nome do campo no formulário</th>
-                    <th>Tipo do campo no formulário</th>
-                    <th>Tipo de unidade</th>
-                    <th>Ordem do campo no formulário</th>
-                    <th>Tamanho do campo no formulário</th>
-                    <th>Obrigatório</th>
-                    <th>Estado</th>
-                    <th>Ação</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <?php
-                        $selecionaProp = "SELECT * FROM property WHERE rel_type_id = NULL";
-                        $resultSeleciona = $db->runQuery($selecionaProp);
-                        $numLinhas = $resultSeleciona->num_rows();
-                        while($resultSeleciona->fetch_assoc())
-                        {
-                            $ent_type_id = $resultSeleciona["ent_type_id"];
-                            $idProp = $resultSeleciona["id"];
-                            $nome = $resultSeleciona["ent_type_id"];
-                            $value_type = $resultSeleciona["value_type"];
-                            $form_field_name = $resultSeleciona["form_field_name"];
-                            $form_field_type = $resultSeleciona["form_field_type"];
-                            $unit_type_id = $resultSeleciona["unit_type_id"];
-                            $form_field_order = $resultSeleciona["form_field_order"];
-                            $form_field_size = $resultSeleciona["form_field_size"];
-                            $mandatory = $resultSeleciona["mandatory"];
-                            $state = $resultSeleciona["state"];                            
-                        }
-                    ?>
-                    <td rowspan="<?php echo $numLinhas?>"></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
-    </html>
-<?php
-    }
-    else
-    {
-        
+        }
     }
 }
-
-
 ?>
