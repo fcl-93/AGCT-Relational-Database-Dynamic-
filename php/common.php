124247cc34df1810dfcc23da94cdc7d85d30df6b
vmcbaptista <2054313@student.uma.pt>
2016-03-11 18:19:52 +0000

    echos debuging

--- php/common.php
+++ php/common.php
@@ -25,7 +25,7 @@ class Db_Op
     public function runQuery($query)
     {
         echo $query;
-        var_dump($this->mysqli);
+        var_dump((get_object_vars($this));
     	$result = $this->mysqli->query($query);
 	    if(!$result)
 	    {
