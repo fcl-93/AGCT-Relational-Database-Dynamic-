a7b554fe2c2e4e505573fc633ceab618794bcb5d
Fabio <fabiocl93@gmail.com>
2016-03-12 14:24:20 +0000

    Validações 2 Forms

--- js/enTypeFormValid.js
+++ js/enTypeFormValid.js
@@ -9,6 +9,15 @@ $().ready(function(){
 			atv_int:"Por favor selecione o estado da entidade"
 		}
 	});
-	$("#editForm")
+	$("#editForm").validate({
+		rules:{
+			nome:"required",
+			atv_int: "required"
+		},
+		messages:{
+			nome:"Por favor introduza o nome da entidade",
+			atv_int:"Por favor selecione o estado da entidade"
+		}
+	});
 		
 });
