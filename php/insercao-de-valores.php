9a5559a0b53f41ad0639d488a5cf90a19f81c87a
vmcbaptista <2054313@student.uma.pt>
2016-03-14 21:29:58 +0000

    insercao de valores - estado introducao

--- php/insercao-de-valores.php
+++ php/insercao-de-valores.php
@@ -171,7 +171,7 @@ class InsertValues{
        $execQueryProp = $this->db->runQuery($queryProp);
        while ($arrayProp = $execQueryProp->fetch_assoc())
        {
-            $queryUn = "SELECT * FROM prop_unit_type AS put, property AS prop WHERE put.id = ".$arrayProp["unit_type_id"];
+            $queryUn = "SELECT name FROM prop_unit_type AS put WHERE put.id = ".$arrayProp["unit_type_id"];
             $resUn = $this->db->runQuery($queryUn);
             $un = $resUn->fetch_assoc();
 ?>
