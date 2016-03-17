756a8e8680ce6fdf625e858589a1463c13787426
vmcbaptista <2054313@student.uma.pt>
2016-03-17 18:24:06 +0000

    alterações no css e no js das tabelas mudança do css para class criacao de um novo file js para o sort das tabelas

--- php/common.php
+++ php/common.php
@@ -6,6 +6,7 @@ $VAL_CLIENT_SIDE=0; // variable that controls the client side validations. If 1 
 <html>
 	<head>
             <script type="text/javascript" src="<?php echo get_bloginfo('wpurl');?>/custom/js/jquery-1.12.1.js"></script> 
+            <script type="text/javascript" src="<?php echo get_bloginfo('wpurl');?>/custom/js/jquery.tablesorter.min.js"></script>
             <script type="text/javascript" src="<?php echo get_bloginfo('wpurl');?>/custom/js/tableSorter.js"></script>
 <?php
         if ($VAL_CLIENT_SIDE)
