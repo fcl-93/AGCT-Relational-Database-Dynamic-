097a602c7c42f5afe552689960bdbcefe898b00a
Fabio <fabiocl93@gmail.com>
2016-03-17 21:33:02 +0000

    Validações Server Side

--- php/gestao-de-formularios.php
+++ php/gestao-de-formularios.php
@@ -378,7 +378,7 @@ class gereForms
                         </thead>
                         <tbody>    
 <?php			
-                        $res_Ent = $this->bd->runQuery('SELECT * FROM ent_type');
+                        $res_Ent = $this->bd->runQuery("SELECT DISTINCT ent_type.id, ent_type.name FROM ent_type , property WHERE property.ent_type_id=ent_type.id");
 			while($arrayRes = $res_Ent->fetch_assoc())
 			{
 				$res_getProp = $this->bd->runQuery("SELECT p.id, p.name, p.value_type, p.form_field_name, p.form_field_type, p.unit_type_id, p.form_field_order, p.mandatory, p.state FROM property AS p, ent_type AS e WHERE  p.ent_type_id = e.id AND e.name LIKE '".$arrayRes["name"]."' ORDER BY p.name ASC");
