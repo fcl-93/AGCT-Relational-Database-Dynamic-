9f117d610c9d1cd053122243bfc7349d49d9b008
Fabio <fabiocl93@gmail.com>
2016-03-19 17:23:51 +0000

    DEBUG prop allowed values

--- php/gestao-de-valores-permitidos.php
+++ php/gestao-de-valores-permitidos.php
@@ -137,7 +137,7 @@ class ValoresPermitidos
 							if($conta == 0)
 							{
 ?>
-								<td rowspan='<?php echo $acerta->num_rows; ?>'><?php echo $read_EntName['name'];?></td>
+								<td rowspan='<?php echo $acerta->num_rows + $acerta2->num_rows; ?>'><?php echo $read_EntName['name'];?></td>
 <?php 	
 								$printedNames[] = $read_EntName['name'];
 							}
