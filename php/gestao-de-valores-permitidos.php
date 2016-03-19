fc5629e539b58dc42072baff004bd931fdf9de84
Fabio <fabiocl93@gmail.com>
2016-03-19 15:47:50 +0000

    DEBUG prop allowed values

--- php/gestao-de-valores-permitidos.php
+++ php/gestao-de-valores-permitidos.php
@@ -265,7 +265,7 @@ class ValoresPermitidos
                             
                             $res_name1 = $this->bd->runQuery("SELECT * FROM ent_type WHERE id=".$read_EntName['ent_type1_id']);
                             $read_name1 = $res_name1->fetch_assoc();
-                            $res_name2 = $this->bd->runQuery("SELECT * FROM ent_type WHERE id=".$read_EntName['ent_type2_id'])
+                            $res_name2 = $this->bd->runQuery("SELECT * FROM ent_type WHERE id=".$read_EntName['ent_type2_id']);
                             $read_name2 = $res_name2->fetch_assoc();
                                                                     
                             //Get the number of properties with that belonh to the etity I'm printing and have enum tipe
