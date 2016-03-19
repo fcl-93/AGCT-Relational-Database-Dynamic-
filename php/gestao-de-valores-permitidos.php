cb5a0d2f75128cb50e7b0cc0fe93aa03d6db736a
Fabio <fabiocl93@gmail.com>
2016-03-19 15:21:17 +0000

    DEBUG prop allowed values

--- php/gestao-de-valores-permitidos.php
+++ php/gestao-de-valores-permitidos.php
@@ -81,6 +81,9 @@ class ValoresPermitidos
 	public function tablePrintEntities()
 	{
 		// gets all properties with enum in value_type.
+?>
+                        <h3>Gestão de valores permitidos - Entidades</h3>
+<?php
 		$res_NProp = $this->bd->runQuery("SELECT * FROM property WHERE value_type = 'enum' AND rel_type_id IS NULL"); 
 		$num_Prop = $res_NProp->num_rows;
 		if($num_Prop > 0)
@@ -220,6 +223,156 @@ class ValoresPermitidos
 <?php 						
 		}
 	}
+        
+        
+        public function tablePrintRelation(){
+?>
+            <h3>Gestão de valores permitidos - Relações</h3>
+<?php
+            $res_NProp = $this->bd->runQuery("SELECT * FROM property WHERE value_type = 'enum' AND ent_type_id IS NULL"); 
+            $num_Prop = $res_NProp->num_rows;
+            if($num_Prop->num_rows > 0)
+            {
+?>
+            <html>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Entidade</th>
+                            <th>Id</th>
+                            <th>Propriedade</th>
+                            <th>Id</th>
+                            <th>Valores permitidos</th>
+                            <th>Estado</th>
+                            <th>Ação</th>
+                        <tr>
+                    </thead>
+                    <tbody>
+<?php
+                    $printedNames = array();
+                    while($read_PropWEnum = $res_NProp->fetch_assoc())
+                    {
+?>
+                        <tr>
+<?php
+                            //Get all enum values for the property that in will start printing now
+                            $res_Enum = $this->bd->runQuery("SELECT * FROM prop_allowed_value WHERE property_id=".$read_PropWEnum['id']);
+                                                                    
+                            //Get the entity name and id that is related to the property we are printing
+                            $res_Ent = $this->bd->runQuery("SELECT id, name FROM ent_type WHERE id = ".$read_PropWEnum['ent_type_id']);
+                            $read_EntName = $res_Ent->fetch_assoc();
+                                                                    
+                            //Get the number of properties with that belonh to the etity I'm printing and have enum tipe
+                            $res_NumProps= $this->bd->runQuery("SELECT * FROM property WHERE ent_type_id = ".$read_PropWEnum['ent_type_id']." AND value_type = 'enum'");
+                                                                    
+                            //Get all the enum values that we wil print this is only the number.
+                            $acerta = $this->bd->runQuery("SELECT * FROM prop_allowed_value as pav ,property as prop, ent_type as ent WHERE ent.id = ".$read_EntName['id']." AND  prop.ent_type_id = ".$read_EntName['id']." AND prop.value_type = 'enum' AND prop.id = pav.property_id");
+                                                                    
+                            //verifies if the name i'm printing has ever been written
+                            $conta = 0;
+                            for($i = 0; $i < count($printedNames); $i++)
+                            {
+				if($printedNames[$i] == $read_EntName['name'])
+				{
+                                    $conta++;
+				}
+                                                             
+                            }
+                                                        
+                            if($conta == 0)
+                            {
+?>
+                            <td rowspan='<?php echo $acerta->num_rows; ?>'><?php echo $read_EntName['name'];?></td>
+<?php 	
+								$printedNames[] = $read_EntName['name'];
+							}
+							else
+							{
+								//echo '<td rowspan='.mysqli_num_rows($acerta).'>';	
+                                                                    
+							}
+?>
+                            <td rowspan="<?php echo $res_Enum->num_rows;?>"><?php echo $read_PropWEnum['id'];?></td>
+                            <!-- Nome da propriedade -->
+                            <td rowspan="<?php echo $res_Enum->num_rows;?>"><a href="gestao-de-valores-permitidos?estado=introducao&propriedade=<?php echo $read_PropWEnum['id'];?>">[<?php echo $read_PropWEnum['name'];?>]</a></td>
+                                
+<?php 							
+							//$propAllowedArray = mysqli_fetch_assoc($propAllowed);
+							if($res_Enum->num_rows == 0)
+							{
+?>
+                            <td colspan=4> Não há valores permitidos definidos </td>
+<?php 
+							}
+							else
+							{
+								while($read_EnumValues = $res_Enum->fetch_assoc())
+								{			
+?>									
+                            <td><?php  echo $read_EnumValues['id'];?></td>
+                            <td><?php echo $read_EnumValues['value'];?></td>
+                            <td>
+<?php 			
+										if($read_EnumValues['state'] == 'active')
+										{
+?>
+                                Ativo
+<?php 
+										}
+										else 
+										{
+?>	
+                                Inativo
+<?php 											
+										}
+                                                                                    
+?>										
+                            </td>
+                            <td>
+                                <a href="gestao-de-valores-permitidos?estado=editar&enum_id=<?php echo $read_EnumValues['id'];?>&prop_id=<?php echo $read_PropWEnum['id'];?>">[Editar]</a>  
+<?php 
+										if($read_EnumValues['state'] === 'active')
+										{
+?>
+                                <a href="gestao-de-valores-permitidos?estado=desativar&enum_id=<?php echo $read_EnumValues['id'];?>">[Desativar]</a>
+<?php 
+										}
+										else 
+										{
+?>
+                                <a href="gestao-de-valores-permitidos?estado=ativar&enum_id=<?php echo $read_EnumValues['id'];?>">[Ativar]</a>
+<?php 
+										}
+?>										
+                            </td>
+                        </tr>		
+<?php 								
+								}
+							}
+?>
+                        </tr>
+<?php 
+						}
+?>
+                    </tbody>
+                </table>
+            </html>    
+<?php
+            }
+            else
+            {
+?>
+                        <html>
+				<p>Não há propriedades especificadas cujo tipo de valor seja enum. <br>
+				Especificar primeiro nova(s) propriedade(s) e depois voltar a esta opção</p>
+			</html>
+<?php                
+            }
+        }
+        
+        
+        
+        
 	/**
 	 * This method will print the for to insert new enum values.
 	 */
