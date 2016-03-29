5bfa3b1bb3f9132199b0153d465eee70d2e93360
vmcbaptista <2054313@student.uma.pt>
2016-03-29 21:04:21 +0100

    importação de valores - validacao extra do form_field_size

--- php/insercao-de-valores.php
+++ php/insercao-de-valores.php
@@ -164,6 +164,7 @@ class InsertValues{
 ?>
         <h3>Inserção de valores - <?php echo $_SESSION[$tipo."_name"];?></h3>
         <form method="POST" name="<?php echo $tipo."_".$_SESSION[$tipo."_id"];?>" action="?estado=validar&<?php echo $tipo;?>=<?php echo $_SESSION[$tipo."_id"];?>">
+            <p class="mandatory">Os campos marcados com * são de preenchimento obrigatorio</p>
 <?php
        if ($tipo === "ent"){
            $queryProp = "SELECT * FROM property WHERE ent_type_id = ".$_SESSION[$tipo."_id"]." AND state = 'active' ORDER BY form_field_order ASC";
@@ -180,16 +181,36 @@ class InsertValues{
            
            if ($tipo === "ent") {
 ?>
-            <label><?php echo $arrayProp["name"];?></label><br>
+            <label><?php echo $arrayProp["name"];?>
 <?php
+                if ($arrayProp["mandatory"] == 1) {
+?>
+                     *</label><br>
+<?php 
+                }
+                else {
+?>
+                     </label><br>
+<?php 
+                }
            }
            else {
                $getEntidade = "SELECT * FROM ent_type WHERE id = ".$arrayProp["ent_type_id"];
                $entidade = $this->db->runQuery($getEntidade)->fetch_assoc();
                $arrayEntidades[$entidade["id"]]=$entidade["name"];
 ?>
-            <label><?php echo $entidade["name"]."-".$arrayProp["name"];?></label><br>
-<?php   
+            <label><?php echo $entidade["name"]."-".$arrayProp["name"];?>
+<?php
+                if ($arrayProp["cfhp.mandatory"] == 1) {
+?>
+                     *</label><br>
+<?php 
+                }
+                else {
+?>
+                     </label><br>
+<?php 
+                }
            }
             switch ($arrayProp["value_type"])
             {
