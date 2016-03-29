3c1b110a563b4b0a6ead8fcc8868ea7ddbe402b0
Fabio <fabiocl93@gmail.com>
2016-03-29 15:39:28 +0100

    Adição do Pager

--- php/common.php
+++ php/common.php
@@ -16,6 +16,9 @@ $VAL_CLIENT_SIDE=1; // variable that controls the client side validations. If 1 
             
             <script type="text/javascript" src="<?php echo get_bloginfo('wpurl');?>/custom/js/jquery.validate.js"></script>
             <script type="text/javascript" src="<?php echo get_bloginfo('wpurl');?>/custom/js/formValidation.js"></script>
+            <script> 
+                $('.table').paging({limit:5});
+            </script>
 <?php
         }
 ?>
