9cbf39b8c71a1b744241e15d9ec6ea10f0caf1c3
Fabio <fabiocl93@gmail.com>
2016-03-31 17:26:34 +0100

    JavaScript

--- php/insercao-de-relacoes.php
+++ php/insercao-de-relacoes.php
@@ -134,7 +134,7 @@ class InsereRelacoes
                                              	{
                                              		$nome = $this->bd->runQuery("SELECT * FROM property WHERE id=".$read_Props['property_id'])->fetch_assoc()['name'];
 ?>
-													<p class="hidden"><?php echo $nome." : ".$read_Props['value']; ?></p>										
+													<p hidden="hidden"><span class="hidden"><?php echo $nome." : ".$read_Props['value']; ?></span></p>										
 <?php
                                              	}
 ?>                                           
@@ -150,7 +150,7 @@ class InsereRelacoes
                                              {
                                              	$nome = $this->bd->runQuery("SELECT * FROM property WHERE id=".$read_Props['property_id'])->fetch_assoc()['name'];
 ?>
-												<p class="hidden"><?php echo $nome." : ".$read_Props['value']; ?></p>												
+												<p hidden="hidden"><span class="hidden"><?php echo $nome." : ".$read_Props['value']; ?></span></p>												
 <?php
                                              }
 ?>                         
