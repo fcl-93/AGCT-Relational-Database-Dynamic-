7fa473dcf03538dc67ec390d5607e264229c3d4f
Fabio <fabiocl93@gmail.com>
2016-03-31 18:52:22 +0100

    Ballon adding

--- js/fetch-db.js
+++ js/fetch-db.js
@@ -1,6 +1,6 @@
 $().ready(function(){
    $("[data-showHidden]").mouseover(function(e){
 	   	$var = $(this).find("p span").text();
-        $(this).balloon({ contents: $var},{ position: "bottom right" });
+        $(this).balloon();
     });
 });
