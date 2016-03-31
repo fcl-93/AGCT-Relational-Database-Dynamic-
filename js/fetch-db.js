4916e716ecb187ed3a806539c90ecf96814fce0c
Fabio <fabiocl93@gmail.com>
2016-03-31 20:06:44 +0100

    Ballon adding

--- js/fetch-db.js
+++ js/fetch-db.js
@@ -1,9 +1,7 @@
 $().ready(function(){
    $("[data-showHidden]").mouseover(function(e){
 	var x = $(this).find("p span").text();
-        $("[data-showHidden] [hidden]").balloon({ position: "null",  contents: '<a href="#">Any HTML!</a><br />'
-    +'<input type="text" size="40" />'
-    +'<input type="submit" value="Search" />'});
+        $("[data-showHidden] [hidden]").balloon({ position: "null",  contents: x});
         //alert(x);mouseover
    });
 });
