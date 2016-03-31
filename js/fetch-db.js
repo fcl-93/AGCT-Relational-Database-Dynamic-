fc530a495e247b5a24e7daea9b3890a80baed842
Fabio <fabiocl93@gmail.com>
2016-03-31 20:04:19 +0100

    Ballon adding

--- js/fetch-db.js
+++ js/fetch-db.js
@@ -1,7 +1,7 @@
 $().ready(function(){
    $("[data-showHidden]").mouseover(function(e){
 	var x = $(this).find("p span").text();
-        $("[data-showHidden] [hidden]").balloon({ position: "null", content: "Olá Olé Oli Olo Olu" });
+        $("[data-showHidden] [hidden]").balloon({ position: "null", content: "Olá Olé Oli Olo Olu", css: z-index="9999"});
         //alert(x);mouseover
    });
 });
