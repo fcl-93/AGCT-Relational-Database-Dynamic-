60e34dbc05b2c5d7fa4c35464b3eb47abf5fb3c0
Fabio <fabiocl93@gmail.com>
2016-03-31 20:09:50 +0100

    Ballon adding

--- js/fetch-db.js
+++ js/fetch-db.js
@@ -1,6 +1,6 @@
 $().ready(function(){
    $("[data-showHidden]").mouseover(function(e){
-	var x = $(this).find("p span").text();
+	var x = $(this).find("span").text();
         $(this).balloon({ position: "null",  contents: x});
         //alert(x);mouseover
    });
