c3bbcdf6971eb8d5be7b21efe72bbedd33253b53
Fabio <fabiocl93@gmail.com>
2016-04-01 22:30:20 +0100

    Serialization

--- php/getAttr.php
+++ php/getAttr.php
@@ -15,15 +15,25 @@ class FetchData{
 	public function getData(){
                 $sanitizeId = $this->bd->userInputVal($_REQUEST['ent']);
 		$res_Props = $this->bd->runQuery("SELECT * FROM value WHERE entity_id=".$sanitizeId);
-
-		while($read_Props = $res_Props->fetch_assoc())
-		{
-			$nome = $this->bd->runQuery("SELECT * FROM property WHERE id=".$read_Props['property_id'])->fetch_assoc()['name'];
-                        
- ?>
-                        <p><span id="results"><?php echo $nome . " : " .$read_Props['value']."\n";?></span></p>
+                if($res_Props->num_rows == 0)
+                {
+?>
+                    <p><span id="results">"A entidade selecionada não tem propriedades associadas."</span></p>
 <?php
-		}
+                }
+                else 
+                {
+                               while($read_Props = $res_Props->fetch_assoc())
+                               {
+                                       $nome = $this->bd->runQuery("SELECT * FROM property WHERE id=".$read_Props['property_id'])->fetch_assoc()['name'];
+
+?>
+                                       <p><span id="results"><?php echo $nome . " : " .$read_Props['value']."\n";?></span></p>
+<?php
+                               }
+
+                }
+
 
                 
 	}
