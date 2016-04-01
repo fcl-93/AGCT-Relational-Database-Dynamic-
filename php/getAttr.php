ae2d1718ca3b5c31f3848e4b1b7daafa6a0895b0
Fabio <fabiocl93@gmail.com>
2016-04-01 21:19:30 +0100

    Serialization

--- php/getAttr.php
+++ php/getAttr.php
@@ -15,13 +15,18 @@ class FetchData{
 	public function getData(){
                 $sanitizeId = $this->bd->userInputVal($_REQUEST['ent']);
 		$res_Props = $this->bd->runQuery("SELECT * FROM value WHERE entity_id=".$sanitizeId);
-
+?>
+            <div id="results">
+<?php
 		while($read_Props = $res_Props->fetch_assoc())
 		{
 			$nome = $this->bd->runQuery("SELECT * FROM property WHERE id=".$read_Props['property_id'])->fetch_assoc()['name'];
                         
-                        echo $nome . " : " .$read_Props['value']."</br>";
+                        echo $nome . " : " .$read_Props['value']."\n";
 		}
+?>
+            </div>
+<?php
                 
 	}
 }
