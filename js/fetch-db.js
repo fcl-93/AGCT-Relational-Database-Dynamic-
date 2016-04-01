d9a7bb4074d9d8048d1e95f8fabd02d852e68891
Fabio <fabiocl93@gmail.com>
2016-04-01 17:50:57 +0100

    Site URL Test

--- js/fetch-db.js
+++
@@ -1,7 +0,0 @@
-$().ready(function(){
-   $("[data-showHidden]").mouseover(function(e){
-	var x = $(this).find("span").text();
-        x = x.replace(/\n/g, '<br>');
-        $(this).balloon({ position: "null",  contents: x});
-   });
-});
