e33e502a3d91da57497b3b604ccf94b6c7710cfd
Fabio <fabiocl93@gmail.com>
2016-04-01 17:50:10 +0100

    Site URL Test

--- js/showAttr.js
+++ js/showAttr.js
@@ -1,6 +1,24 @@
 $().ready(function(){
     $("#ent").change(function(){
-        var id = $("#ent").val();
-        alert("O id e : " + id);
+        var idEnt = $("#ent").val();
+        $.ajax({
+ 
+    // The URL for the request
+    url: "getAttr",
+ 
+    // The data to send (will be converted to a query string)
+    data: {
+        id: idEnt
+    },
+ 
+    // Whether this is a POST or GET request
+    type: "GET",
+ 
+    // The type of data we expect back
+    dataType : "json",
+}).done(function(json){
+    alert("Recebido");
+});
+        //alert("O id e : " + id);
     });
 });
