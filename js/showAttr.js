e4715fb60cf93a976d7e1123e169cb6c6a4bb9fd
Fabio <fabiocl93@gmail.com>
2016-04-01 20:48:17 +0100

    Serialization

--- js/showAttr.js
+++ js/showAttr.js
@@ -3,26 +3,26 @@ $().ready(function(){
         var idEnt = $("#ent").val();
         $.ajax({
  
-    // The URL for the request
-    url: "/getAttr",
- 
-    // The data to send (will be converted to a query string)
-    data: {
-        id: idEnt
-    },
- 
-    // Whether this is a POST or GET request
-    type: "GET",
- 
-    // The type of data we expect back
-    dataType : "json",
-    
-    success: function(data) {
-        alert("Sucesso");
-    },
-    error: function(xhr, desc, err) {
-        alert(xhr + " details " + desc + " error: " + err);
-    }
+        // The URL for the request
+        url: "/getAttr",
+
+        // The data to send (will be converted to a query string)
+        data: {
+            id: idEnt
+        },
+
+        // Whether this is a POST or GET request
+        type: "GET",
+
+        // The type of data we expect back
+        dataType : "json",
+
+        success: function(data) {
+            alert("Sucesso");
+        },
+        error: function(xhr, desc, err) {
+            alert(xhr + " details " + desc + " error: " + err);
+        }
 
 });
         //alert("O id e : " + id);
