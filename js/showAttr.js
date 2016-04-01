019ca4da8d8b31eb6dfdd1cd8dd8570c73c7bfbe
Fabio <fabiocl93@gmail.com>
2016-04-01 21:07:18 +0100

    Serialization

--- js/showAttr.js
+++ js/showAttr.js
@@ -1,14 +1,14 @@
 $().ready(function(){
     $("#ent").change(function(){
-        var idEnt = $("#ent").val();
-        var data = {
-            'id': idEnt
-        };
-
-        // The variable ajax_url should be the URL of the admin-ajax.php file
-        $.post( admin_url( 'getAttr.php' ), data, function(response) {
-            console.log( response );
-        }, 'json');
-        
+        e.preventDefault(); 
+        var url = $("#ent").val();
+        $.ajax({
+ 
+        // The URL for the request
+        url: url,
+}).done(function( html ) {
+    alert("Enviei e recebi")
+});
+        //alert("O id e : " + id);
     });
 });
