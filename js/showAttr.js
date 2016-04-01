763de4cb16d0211842afb113a20f37ecde768c51
vmcbaptista <2054313@student.uma.pt>
2016-04-01 20:42:53 +0100

    ajax

--- js/showAttr.js
+++ js/showAttr.js
@@ -4,7 +4,7 @@ $().ready(function(){
         $.ajax({
  
     // The URL for the request
-    url: "/getAttr",
+    url: "/wp-admin/test.php",
  
     // The data to send (will be converted to a query string)
     data: {
