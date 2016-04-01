939255d7599eb93f8301f538cedeea3525fde855
Fabio <fabiocl93@gmail.com>
2016-04-01 18:42:48 +0100

    Serialization

--- php/getAttr.php
+++ php/getAttr.php
@@ -14,7 +14,7 @@ class FetchData{
 	
 	public function getData(){
             //Decode Json
-            echo get_site_url();
+            echo $_SERVER['PHP_SELF'];
             $json = file_get_contents(get_site_url());
             $obj = json_decode($json,TRUE);
             echo $obj['data']['id'];
