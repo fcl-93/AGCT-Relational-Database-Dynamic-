bd1f92bca925506a43afa5476c0619e22ae2ff37
vmcbaptista <2054313@student.uma.pt>
2016-04-01 20:45:25 +0100

    ajax

--- js/showAttr.js
+++ js/showAttr.js
@@ -4,7 +4,7 @@ $().ready(function(){
         $.ajax({
  
     // The URL for the request
-    url: "<?php bloginfo('url'); ?>/wp-admin/test.php",
+    url: "http://agct16.ddns.net/wp-admin/test.php",
  
     // The data to send (will be converted to a query string)
     data: {
