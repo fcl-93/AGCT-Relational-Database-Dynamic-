36676af39471c32421bbeaa50c5686a1a60e57c1
Fabio <fabiocl93@gmail.com>
2016-04-02 18:53:52 +0100

    Adição de Text Quando js está inativo

--- php/insercao-de-relacoes.php
+++ php/insercao-de-relacoes.php
@@ -561,7 +561,7 @@ class InsereRelacoes
             ?>
                                     <tr>
                                         <td><input type="checkbox" name="secondEnt<?php echo $control; ?>" value="<?php echo $read_SecondEnt['id'];?>"><?php echo $read_SecondEnt['entity_name']; ?></td>
-                                        <td><label>Nome para a relação </label><input type="text" name ="nomeDaRel<?php echo $control; ?>"></td>
+                                        <td><!--<label>Nome para a relação </label>--><input type="text" name ="nomeDaRel<?php echo $control; ?>"></td>
                                     <tr>
             <?php
                                     }
@@ -620,6 +620,8 @@ class InsereRelacoes
                             }
                             $_SESSION['numEnt2Max'] = $control; 
         ?>
+                                    </tbody>
+                        </table>
 
                              <input type="hidden" name="rel_type" value="<?php echo $sltd_RelType;?>">
                             <input type="hidden" name="firstEnt" value="<?php echo  $prev_SelEnt?>">
@@ -627,8 +629,6 @@ class InsereRelacoes
                             <input type="hidden" name="flag" value="naoeditar">
                             <input type="hidden" name="estado" value="inserir">
                             <input type="submit" value="Associar Segunda Entidade">
-                            </tbody>
-                        </table>
                     </form>
                 </html>
 <?php                              
