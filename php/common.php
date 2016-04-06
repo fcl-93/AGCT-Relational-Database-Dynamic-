f1964d15231a23b6686568bd29ccb7fd0cb588a1
vmcbaptista <2054313@student.uma.pt>
2016-04-06 14:23:07 +0100

    pesquisa dinâmica possivelmente com combincações entre tabelas

--- php/common.php
+++ php/common.php
@@ -19,7 +19,7 @@ $VAL_CLIENT_SIDE=1; // variable that controls the client side validations. If 1 
             
             <script type="text/javascript" src="<?php echo get_bloginfo('wpurl');?>/custom/js/jquery.tablesorter.pager.js"></script>
 
-            <script type="text/javascript" >
+            <script type="text/javascript" defer = "defer" >
             
             $(document).ready(function() { 
                 $("table").tablesorterPager({container: $("#pager")}); 
