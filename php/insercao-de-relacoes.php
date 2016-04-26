8cdf726f112e960a8ba2755bb6df79226cb8df48
Fabio <fabiocl93@gmail.com>
2016-04-26 20:23:12 +0100

    Adição do historico desativações e ativações

--- php/insercao-de-relacoes.php
+++ php/insercao-de-relacoes.php
@@ -1191,7 +1191,7 @@ class RelHist{
                   }
                   else
                   {
-                      if(!$bd->runQuery("INSERT INTO `hist_value`(`id`, `entity_id`, `property_id`, `value`, `producer`, `relation_id`, `value_id`, `active_on`, `inactive_on`, `state`) VALUES (NULL,".$readSVal['entity_id'].",".$readSVal['property_id'].",".$readSVal['value'].",'".$readSVal['producer']."',".$id.",".$readSVal['id'].",'".$readSVal['updated_on']."','".$inactive."','".$readSVal['state']."')"))
+                      if(!$bd->runQuery("INSERT INTO `hist_value`(`id`, `entity_id`, `property_id`, `value`, `producer`, `relation_id`, `value_id`, `active_on`, `inactive_on`, `state`) VALUES (NULL,".$readSVal['entity_id'].",".$readSVal['property_id'].",'".$readSVal['value']."','".$readSVal['producer']."',".$id.",".$readSVal['id'].",'".$readSVal['updated_on']."','".$inactive."','".$readSVal['state']."')"))
                       {
                         return false;
                       }
