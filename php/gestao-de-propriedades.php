65bd5fdc82ae33d342f8a08eb2ed52e584b6d053
vmcbaptista <2054313@student.uma.pt>
2016-05-01 00:09:27 +0100

    historico propriedades

--- php/gestao-de-propriedades.php
+++ php/gestao-de-propriedades.php
@@ -1119,7 +1119,7 @@ class PropHist{
                     }
 ?>
                     </td>
-                    <td><a href ="?estado=voltar&hist=<?php echo $hist["id"];?>&prop=<?php echo $_REQUEST["id"];?>">Voltar para esta versão</a></td>
+                    <td><a href ="?estado=voltar&hist=<?php echo $hist["id"];?>&prop_id=<?php echo $_REQUEST["id"];?>">Voltar para esta versão</a></td>
                 </tr>
 <?php
         }
