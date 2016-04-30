a77db95a97b588f853e0129a020a9321a2602d21
Fabio <fabiocl93@gmail.com>
2016-04-30 15:23:10 +0100

    Edição de formulários para o historico - Fase Testes

--- php/gestao-de-formularios.php
+++ php/gestao-de-formularios.php
@@ -338,6 +338,7 @@ class GereForms
                         return false;
                     }
                     //
+                    $numOrdem = array();
                     for($i = 1; $i <= $_SESSION['propSelected']; $i++)
                     {
                             if((!is_numeric($_REQUEST["ordem".$i]) || $_REQUEST["ordem".$i] < 1) && isset($_REQUEST["idProp".$i]))
@@ -356,9 +357,20 @@ class GereForms
 <?php 
                                 return false;
                             }
+                            array_push($numOrdem, $_REQUEST['ordem'.$i]);
+                    }
+                    //Check if there is any duplicated value in order (campo ordem) field
+                    $duplicatedValueChecka = array_count_values($numOrdem);
+                    foreach ($duplicatedValueChecka as $key => $value) {
+                        if($value > 1)
+                        {
+?>
+                                <p>Os campos ordem não devem ter valores iguais.</p><br>
+<?php
+                        return false;
+                        }
                     }
                     return true;
-
                 }	
                 else 
                 {
@@ -683,10 +695,7 @@ class GereForms
             }
             else
             {
-?>
-                   <p>O seu formulário não pôde ser atualizado porque ocorreu um erro.</p>
-                   <p>Clique em <?php goBack(); ?></p>  
-<?php
+                   goBack(); 
             }
 
         }
