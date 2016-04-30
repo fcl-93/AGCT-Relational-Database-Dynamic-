2258e4025b0e990db2c6d36d7947f28c851cef4a
vmcbaptista <2054313@student.uma.pt>
2016-04-30 22:56:32 +0100

    pager

--- php/common.php
+++ php/common.php
@@ -12,8 +12,6 @@ $VAL_CLIENT_SIDE=1; // variable that controls the client side validations. If 1 
             <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('wpurl');?>/custom/css/form.css">
             <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('wpurl');?>/custom/css/jquery-ui.css">
             
-            
-            <script type="text/javascript" src="<?php echo get_bloginfo('wpurl');?>/custom/js/jquery-1.12.1.js"></script> 
             <script type="text/javascript" src="<?php echo get_bloginfo('wpurl');?>/custom/js/jquery-ui.min.js"></script>
             <script type="text/javascript" src="<?php echo get_bloginfo('wpurl');?>/custom/js/jquery.tablesorter.min.js"></script>
             <script type="text/javascript" src="<?php echo get_bloginfo('wpurl');?>/custom/js/jquery.tablesorter.pager.js"></script>
