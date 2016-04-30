a02c0624bb1fb807f33d6277cb4b8af979ee9e3e
Fabio <fabiocl93@gmail.com>
2016-04-30 17:56:25 +0100

    Pesquisa dinâmica adição do historico

--- php/pesquisa-dinamica.php
+++ php/pesquisa-dinamica.php
@@ -1573,6 +1573,10 @@ class Search{
         }
     }
     
+    /**
+     * This method will handle the activation and the the desativation of the
+     * entities.
+     */
     public function changeState(){
         $id = $this->bd->userInputVal($_REQUEST['id']);
         $estado = $this->bd->userInputVal($_REQUEST['estado']);
