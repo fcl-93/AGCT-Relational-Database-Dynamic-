1d309ba07528ff63e21f4e3820510d0878ec31db
vmcbaptista <2054313@student.uma.pt>
2016-04-30 22:02:44 +0100

    pager

--- js/tableSorter.js
+++ js/tableSorter.js
@@ -3,9 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$().ready(function(){
-    $("#sortedTable")
-    .tablesorter()
+$(document).ready(function() { 
+    $("table") 
+    .tablesorter({widthFixed: true, widgets: ['zebra']}) 
     .tablesorterPager({container: $("#pager")}); 
 });
-
