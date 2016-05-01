db9ee839f4c9ff417719a2cec9b3a7ff49983c79
Fabio <fabiocl93@gmail.com>
2016-05-01 08:57:44 +0100

    Pesquisa dinâmica Edição de Propriedades

--- php/pesquisa-dinamica.php
+++ php/pesquisa-dinamica.php
@@ -1831,7 +1831,7 @@ class Search{
 ?>
                     <html>
                         <p>Deve selecionar pelo menos uma propriedade para atualizar</p>
-                        <p>Clique em <?php goBack()?> para voltar à página anterior</p>
+                        <p>Clique em <?php goBack()?> para voltar a página anterior</p>
                     </html>
 <?php
                     return false;
