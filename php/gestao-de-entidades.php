6d55e9d273e15de9d2dc15ea58a147b54a3f16dc
fcl <fcl@fclPC>
2016-05-01 22:40:37 +0100

    Adição da tabela de historico

--- php/gestao-de-entidades.php
+++ php/gestao-de-entidades.php
@@ -510,7 +510,7 @@ class EntHist{
                                {
 ?>
                                 <tr>
-                                    <td clospan="3">Não existe registo referente à entidade selecionada no histórico</td>
+                                    <td colspan="3">Não existe registo referente à entidade selecionada no histórico</td>
                                 </tr>
 <?php
                                }
