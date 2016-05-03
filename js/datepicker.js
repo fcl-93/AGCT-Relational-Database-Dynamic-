a6e1ed3d0d3bd95efcf17702e35681038dec6a4d
vmcbaptista <2054313@student.uma.pt>
2016-05-03 14:29:59 +0100

    datepicker data máxima e formato

--- js/datepicker.js
+++ js/datepicker.js
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 $(function() {
-    $( "#datepicker" ).datepicker();
+    $( "#datepicker" ).datepicker({
+        dateFormat: "yy-mm-dd",
+        maxDate: new Date()
+    });
 });
 
