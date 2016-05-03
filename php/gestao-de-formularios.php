2d0eccfdde2d3f7498de28de1fb3ea93aa52999b
fcl <fcl@fclPC>
2016-05-03 16:55:31 +0100

    Adição da tabela de historico - gestão de formuláios

--- php/gestao-de-formularios.php
+++ php/gestao-de-formularios.php
@@ -777,7 +777,7 @@ class HistDeForms{
                             </thead>
                             <tbody>
 <?php
-                            if($goToCN->num_rows == 0 || $goToCFP->num_rows == 0){
+                            if($goToCFP->num_rows == 0){
 ?>                                
                                 <tr>
                                     <td colspan="6">Não existe registo referente à entidade selecionada no histórico</td>
