a2b4a4953fab0b4998f6974992e8b7a1486ee523
fcl <fcl@fclPC>
2016-05-04 22:07:00 +0100

    Pesquisa DInâmica HIstory

--- php/pesquisa-dinamica.php
+++ php/pesquisa-dinamica.php
@@ -2258,7 +2258,7 @@ class entityHist{
                                     <td><?php echo $readHistory['id']?></td>
                                     <td><?php echo $readHistory['entity_name']?></td>
 <?php                               echo "SELECT * FROM hist_value WHERE entity_id = ".$readHistory['id']." AND inactive_on = '".$readHistory['inactive_on']."'";
-                                    $readHistValues = $bd->runQuery("SELECT * FROM hist_value WHERE inactive_on = '".$readHistory['inactive_on']."'")->fetch_assoc();
+                                    $readHistValues = $bd->runQuery("SELECT * FROM hist_value WHERE inactive_on = '".$readHistory['inactive_on']."'");
                                     while($readHV = $readHistValues->fetch_assoc())
                                     {
                                         $propName = $bd->runQuery("SELECT name FROM property WHERE id=".$readHV['entity_id'])->fetch_assoc();
