d187a2bde61574d81fc75a9d308a4b619cf91b45
vmcbaptista <2054313@student.uma.pt>
2016-05-10 17:28:26 +0100

    ativar desativar

--- php/insercao-de-relacoes.php
+++ php/insercao-de-relacoes.php
@@ -746,7 +746,7 @@ class InsereRelacoes
             $idRel = $this->bd->userInputVal($_REQUEST['rel']);
             if( $this->gereInsRel->addHist($idRel,$this->bd)) {
                 if ($this->gereInsRel->addValHist($idVal,$this->bd)) {
-                    if($this->bd->runQuery("UPDATE value SET updated_on = '".date("Y-m-d H:i:s",time())."' AND state = 'active' WHERE id=".$idVal)) {
+                    if($this->bd->runQuery("UPDATE value SET updated_on = '".date("Y-m-d H:i:s",time())."', state = 'active' WHERE id=".$idVal)) {
 ?>
                         <html>
                            <p>O valor da propriedade foi ativado.</p>
@@ -791,9 +791,7 @@ class InsereRelacoes
             $idRel = $this->bd->userInputVal($_REQUEST['rel']);
             if( $this->gereInsRel->addHist($idRel,$this->bd)) {
                 if ($this->gereInsRel->addValHist($idVal,$this->bd)) {
-                    if($this->bd->runQuery("UPDATE value SET updated_on = '".date("Y-m-d H:i:s",time())."' AND state = 'inactive' WHERE id=".$idVal)) {
-                        echo "UPDATE value SET updated_on = '".date("Y-m-d H:i:s",time())."' AND state = 'inactive' WHERE id=".$idVal;
-                        echo $this->bd->getMySqli()->affected_rows;
+                    if($this->bd->runQuery("UPDATE value SET updated_on = '".date("Y-m-d H:i:s",time())."', state = 'inactive' WHERE id=".$idVal)) {
                         $this->bd->getMysqli()->commit();
 ?>
                         $this->bd->getMysqli()->commit();<html>
@@ -842,7 +840,7 @@ class InsereRelacoes
             $idRel = $this->bd->userInputVal($_REQUEST['rel']);
             if( $this->gereInsRel->addHist($idRel,$this->bd))
             {
-                if($this->bd->runQuery("UPDATE relation SET updated_on = '".date("Y-m-d H:i:s",time())."' AND state = 'active' WHERE id=".$idRel))
+                if($this->bd->runQuery("UPDATE relation SET updated_on = '".date("Y-m-d H:i:s",time())."', state = 'active' WHERE id=".$idRel))
                 {
 ?>
                    <html>
@@ -882,7 +880,7 @@ class InsereRelacoes
              $idRel = $this->bd->userInputVal($_REQUEST['rel']);
             if( $this->gereInsRel->addHist($idRel,$this->bd))
             {
-                if($this->bd->runQuery("UPDATE relation SET updated_on = '".date("Y-m-d H:i:s",time())."' AND state = 'inactive' WHERE id=".$idRel))
+                if($this->bd->runQuery("UPDATE relation SET updated_on = '".date("Y-m-d H:i:s",time())."', state = 'inactive' WHERE id=".$idRel))
                 {
 ?>
                     <html>
