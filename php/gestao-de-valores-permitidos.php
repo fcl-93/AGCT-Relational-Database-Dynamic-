8c3e6031093ac819a5bb68b2b3b6bd5c394f9669
vmcbaptista <2054313@student.uma.pt>
2016-05-15 19:01:10 +0100

    historico valores permitidos

--- php/gestao-de-valores-permitidos.php
+++ php/gestao-de-valores-permitidos.php
@@ -660,12 +660,13 @@ class ValPerHist{
                     }
                 }
                 $updateHist .= " updated_on = '".$updateTime."' WHERE id = ".$old['id'];
-                echo $updateHist;
+                echo $updateHist."<br>";
                 $updateHist =$db->runQuery($updateHist);
                 if ($updateHist) {
-
+                    echo "SELECT * FROM prop_allowed_value WHERE property_id = ".$_REQUEST["prop_id"]." AND updated_on != '".$updateTime."'"."<br>";
                     $selPropOut = $db->runQuery("SELECT * FROM prop_allowed_value WHERE property_id = ".$_REQUEST["prop_id"]." AND updated_on != '".$updateTime."'");
                     while ($propOut = $selPropOut->fetch_assoc()) {
+                        echo "UPDATE prop_allowed_value SET updated_on = '".$updateTime."', state = 'inactive' WHERE id = ".$propOut["id"]."<br>";
                         $updateOut = $db->runQuery("UPDATE prop_allowed_value SET updated_on = '".$updateTime."', state = 'inactive' WHERE id = ".$propOut["id"]);
                         if (!$updateOut) {
 ?>
