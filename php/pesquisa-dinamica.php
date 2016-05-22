39d111aa09a46179dee5408f9fdf3da8ba2351fc
vmcbaptista <2054313@student.uma.pt>
2016-05-22 17:04:22 +0100

    pesquisa dinamica historico todas

--- php/pesquisa-dinamica.php
+++ php/pesquisa-dinamica.php
@@ -2909,8 +2909,8 @@ class entityHist{
 <?php
                 $data = $db->userInputVal($_REQUEST['data']);
                 // Queries that select the verion present in the history or in the main table in the given date
-                $selecionaHist = "SELECT * FROM hist_entity WHERE ('".$data."' > active_on AND '".$data."' < inactive_on) OR ((active_on LIKE '".$data."%' AND inactive_on < '".$data."') OR inactive_on LIKE '".$data."%') GROUP BY entity_id ORDER BY inactive_on DESC";
-                $selecionaEntity = "SELECT * FROM entity WHERE updated_on < '".$data."' OR updated_on LIKE '".$data."%'";
+                $selecionaHist = "SELECT * FROM hist_entity WHERE (('".$data."' > active_on AND '".$data."' < inactive_on) OR ((active_on LIKE '".$data."%' AND inactive_on < '".$data."') OR inactive_on LIKE '".$data."%')) AND ent_type_id = ".$db->userInputVal($_REQUEST["ent_id"])." GROUP BY entity_id ORDER BY inactive_on DESC";
+                $selecionaEntity = "SELECT * FROM entity WHERE (updated_on < '".$data."' OR updated_on LIKE '".$data."%') AND ent_type_id = ".$db->userInputVal($_REQUEST["ent_id"]);
                 echo $selecionaEntity.$selecionaHist;
                 
                 $resultSelecionaEntity = $db->runQuery($selecionaEntity);
