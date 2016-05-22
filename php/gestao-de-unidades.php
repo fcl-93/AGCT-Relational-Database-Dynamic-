2d1c72f06ca23261db56c7fa28a8246e964ff7b1
vmcbaptista <2054313@student.uma.pt>
2016-05-22 10:27:45 +0100

    historico geral unidades

--- php/gestao-de-unidades.php
+++ php/gestao-de-unidades.php
@@ -496,7 +496,7 @@ class UnidadeHist
               
                     
                         $selecionaHist = "SELECT * FROM hist_prop_unit_type WHERE '".$_REQUEST["data"]."' > active_on AND '".$_REQUEST["data"]."' < inactive_on GROUP BY prop_unit_type_id ORDER BY inactive_on DESC";
-                        $selecionaUnit = "SELECT * FROM prop_unit_type WHERE updated_on <= '".$_REQUEST["data"];
+                        $selecionaUnit = "SELECT * FROM prop_unit_type WHERE updated_on <= '".$_REQUEST["data"]."'";
                     
                     
                     $resultSelecionaUnit = $db->runQuery($selecionaUnit);
