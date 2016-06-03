11a91a6c63a18ebbd3ce52e32aaef0c5a721c426
vmcbaptista <2054313@student.uma.pt>
2016-06-03 20:25:58 +0100

    correcao bugs propriedades

--- js/formValidation.js
+++ js/formValidation.js
@@ -47,7 +47,7 @@ $().ready(function(){
 			nome:"Por favor introduza o nome da propriedade.",
                         tipoValor:"Por favor selecione um tipo de valor para a sua entidade.",
                         relacaoPertence:"Por favor selecione a relação a que irá pertencer esta propriedade.",
-                        entidadePertence:"Por favor selecione a relação a que irá pertencer esta propriedade.",
+                        entidadePertence:"Por favor selecione a entidade a que irá pertencer esta propriedade.",
                         tipoCampo:"Por favor selecione um tipo do campo do formulário.",
                         ordem:"Por favor introduza um valor superior a 0.",
                         obrigatorio:"Por favor indique se esta propriedade deve ou não ser obrigatória."
