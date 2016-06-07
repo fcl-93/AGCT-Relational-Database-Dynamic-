f929fb3a755f21360571f107689872508917e8fe
Fabio <fabiocl93@gmail.com>
2016-06-07 11:19:20 +0100

    Mudança dos nomes das entidades, Resolução de Bug com o Active e Inactive

--- php/gestao-de-entidades.php
+++ php/gestao-de-entidades.php
@@ -423,7 +423,7 @@ class Entidade {
 ?>
                     <p>O tipo de entidade <b><?php echo $read_EntTypeD['name'] ?></b>  não pode ser desativado.</p>
                     <p>Uma vez que existem instâncias deste tipo de entidade ativas.</p>
-                    <p>Clique em <a href="/pesquisa-dinamica/?estado=escolha&ent=<?php echo $id?>"/>desativar</a> para pesquisar pelas</p>
+                    <p>Clique em <a href="/pesquisa-dinamica/?estado=execucao&ent=<?php echo $id?>"/>desativar</a> para pesquisar pelas</p>
                     <p>entidades que pretende desativar ou clique em <?php goBack();?> para voltar a página anterior.</p>
 <?php
             }
