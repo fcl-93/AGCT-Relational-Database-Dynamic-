dd44f3435b577840457b3d15779898032374d704
fcl <fcl@fcl-pc>
2016-06-10 17:20:18 +0100

    Gestão de rops Validate

--- js/formValidation.js
+++ js/formValidation.js
@@ -66,9 +66,9 @@ $().ready(function(){
                     }
                     else
                     {
-                        console.log("Erro"); 
+                        console.log("Erro");  $('#errTam').text("Por favor introduza um valor superior a 0.");
                          $('#errTam').addClass("error");
-                       $('#errTam').text("Por favor introduza um valor superior a 0.");
+                      
                     
                        //$('#errTam').css("color","red");
                        //$('#errTam').css("font-style","italic");
