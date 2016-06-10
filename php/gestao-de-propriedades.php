74cb78221223b6ed33c87aca1890971f86bfbd70
vmcbaptista <2054313@student.uma.pt>
2016-06-10 16:12:38 +0100

    estado validar propriedade

--- php/gestao-de-propriedades.php
+++ php/gestao-de-propriedades.php
@@ -655,6 +655,24 @@ class PropertyManage
         }
 ?>
         </ul>
+<?php
+        if(isset($_REQUEST['ent_id']))
+        {
+?>
+            <input type ="hidden" name="entidadePertence" value="<?php echo $_REQUEST['ent_id'];?>">
+<?php
+        }
+        else {
+?>
+            <input type ="hidden" name="relacaoPertence" value="<?php echo $_REQUEST['rel_id'];?>">
+<?php
+        }
+        if (empty($_REQUEST['maisProp'])) {
+?>
+            <input type ="hidden" name="primeiraVez" value="true">
+<?php                    
+        }
+?>
         <input type="hidden" name="estado" value="inserir">
         <input type="submit" value="Submeter">
         </form>
