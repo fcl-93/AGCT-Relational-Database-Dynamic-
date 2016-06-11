8cf6a0c4278fab57e0bba217ca92a81005f21b35
fcl <fcl@fcl-pc>
2016-06-11 17:29:59 +0100

    GESTÃO DE ENTIDADES HISTORICO ENESIMA REFORMULAÇÃO

--- php/gestao-de-entidades.php
+++ php/gestao-de-entidades.php
@@ -645,96 +645,56 @@ class EntHist {
             <?php
         } else {
             while ($readHE = $resHE->fetch_assoc()) {
+                //get properties from the history where the inactive field has an hour equal the ent_type selected
                 $getPropsHist = $bd->runQuery("SELECT * FROM hist_property WHERE ent_type_id = ".$id." AND inactive_on = '".$readHE['inactive_on']."'");
-                
+                //selects a property from properties table where the updated value is smaller than the inactive from the ent_type selected
+                $getProp = $bd->runQuery("SELECT * FROM property WHERE updated_on < '".$readHE['inactive_on']."' AND ent_type_id =".$readHE['ent_type_id']."");
                 $conta = 0;
+                $numlinhas = $getPropsHist->num_rows + $getProp->num_rows;
  ?>
-                    <tr>
-                        <td rowspan="<?php echo $getPropsHist->num_rows?>"><?php echo $readHE['active_on'] ?></td>
-                        <td rowspan="<?php echo $getPropsHist->num_rows?>"><?php echo $readHE['inactive_on'] ?></td>
-                        <td rowspan="<?php echo $getPropsHist->num_rows?>"><?php echo $readHE['name'] ?></td>                   
+                <tr>
+                        <td rowspan="<?php echo $numlinhas ?>"><?php echo $readHE['active_on'] ?></td>
+                        <td rowspan="<?php echo $numlinhas ?>"><?php echo $readHE['inactive_on'] ?></td>
+                        <td rowspan="<?php echo $numlinhas ?>"><?php echo $readHE['name'] ?></td>                   
 <?php
-                        if($getPropsHist->num_rows == 0)
+                        if($numlinhas == 0)
                         {
 ?>
-                        <td colspan="2">Não existem propriedades associadas a este tipo de entidade.</td>
-                        <!--<td>--> <?php /*if($readHE['state'] == 'active')
-                                    {
-                                        echo 'Ativo';
-                        }  else {
-                                        echo 'Inativo';
-                                    }*/?>
-                       <!-- </td>-->
-<?php
-                     
-?>
-                        <td><a href="?estado=versionBack&histId=<?php echo $readHE['id'] ?>">Voltar para esta versão</a></td>
-
+                            <td colspan="3">Não existem propriedades associadas a este tipo de entidade.</td>
+                            <td><a href="?estado=versionBack&histId=<?php echo $readHE['id'] ?>">Voltar para esta versão</a></td>
 <?php
                         }
                         else{
-                        while($propHist = $getPropsHist->fetch_assoc()){
-                            $getProp = $bd->runQuery("SELECT * FROM property WHERE id =".$propHist['property_id']."" )->fetch_assoc();                            
-?>
-                            <td><?php echo $propHist['name']?></td>
-                            <td><?php echo $propHist['value_type']?></td>
-                            <td><?php 
-                            if($propHist['state'] == 'active'){ 
-                                echo "Ativo";
-                                
-                            }else{
-                                echo "Inativo"; 
-                                
-                            }?></td>
+                            if($conta = 0)
+                            {
+                                ?>
+                                    <td <?php echo $numlinhas ?>><a href="?estado=versionBack&histId=<?php echo $readHE['id'] ?>">Voltar para esta versão</a></td>                
 <?php
-                            if($conta == 0){
+                            }
+                            
+                            while($readProp = $getPropsHist->fetch_assoc()){
 ?>
-                            <!--<td rowspan="<?php/* echo $getPropsHist->num_rows*/?>">-->
-                        <?php 
-                          /*          if($readHE['state'] == 'active')
-                                    {
-                                        echo 'Ativo';
-                                    }  else {
-                                        echo 'Inativo';
-                                    }*/
-                        ?>
-                                <!--</td>-->
-                          <?php
-                          /*if($getProp['name'] == $propHist['name'] &&
-                             $getProp['ent_type_id'] == $propHist['ent_type_id'] &&
-                             $getProp['rel_type_id'] == $propHist['rel_type_id'] &&     
-                             $getProp['value_type'] == $propHist['value_type'] &&     
-                             $getProp['form_field_name'] == $propHist['form_field_name'] &&     
-                             $getProp['form_field_type'] == $propHist['form_field_type'] &&
-                             $getProp['unit_type_id'] == $propHist['unit_type_id'] &&
-                             $getProp['form_field_order'] == $propHist['form_field_order'] &&
-                             $getProp['mandatory'] == $propHist['mandatory'] &&
-                             $getProp['state'] == $propHist['state'] &&
-                             $getProp['fk_ent_type_id'] == $propHist['fk_ent_type_id'] &&
-                             $getProp['form_field_size'] == $propHist['form_field_size'] &&
-                             $getProp['id'] == $propHist['property_id'] 
-                                  )
-                          {
-                          */?>
-                        <td rowspan="<?php echo $getPropsHist->num_rows?>"><a href="?estado=versionBack&histId=<?php echo $readHE['id'] ?>">Voltar para esta versão</a></td>
-                          <?php
-                          
-                          /*}
-                          else
-                          {
-?>                          <td rowspan="<?php echo $getPropsHist->num_rows?>">
-                                <p>Não pode voltar para esta versão porque as propriedades que lhe estão atribuidas foram modificadas</p>
-                            </td>
+                                <td><?php echo $readProp['name']?></td>
+                                <td><?php echo $readProp['value_type']?></td>
+                                <td><?php echo $readProp['state']?></td>
 <?php
-                          }*/?>
-<?php   
+                                $conta++;
+?>
+                                </tr>
+<?php
+                            }
+                            while($readProp = $getProp->fetch_assoc()){
+?>
+                                <td><?php echo $readProp['name']?></td>
+                                <td><?php echo $readProp['value_type']?></td>
+                                <td><?php echo $readProp['state']?></td>
+<?php
+                                $conta++;
+?>
+                                </tr>
+<?php
+                            }
                         }
-?>                    
-                        </tr>
-                <?php
-                $conta++;
-            }
-        }
             }
         
     }
