a4b9ae6efcac6bc6b9f54dd1ccfbf3017735f12e
vmcb <vmcb@vmcb-Aspire-E5-571G>
2016-06-18 13:10:07 +0100

    mudanca do historico dos valores permitidos

--- php/gestao-de-valores-permitidos.php
+++ php/gestao-de-valores-permitidos.php
@@ -696,9 +696,6 @@ class ValPerHist{
                 $atributos = $selOldVal->fetch_assoc();
                 $updateHist = "UPDATE prop_allowed_value SET ";
                 foreach ($atributos as $atributo => $valor) {
-                    if ($atributo == 'state') {
-                        $valor = "active";
-                    }
                     if ($atributo != "id" && $atributo != "inactive_on" && $atributo != "active_on" && $atributo != "prop_allowed_value_id" && !is_null($valor)) {
                         $updateHist .= $atributo." = '".$valor."',"; 
                     }
