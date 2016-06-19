599af3717bd22c9dc67763738fc0e09e76f53ad3
vmcbaptista <2054313@student.uma.pt>
2016-06-19 14:44:19 +0100

    mudanças no historico

--- php/gestao-de-propriedades.php
+++ php/gestao-de-propriedades.php
@@ -1493,8 +1493,8 @@ class PropHist{
         <table class="table">
             <thead>
                 <tr>
-                    <th>Data de Ativação</th>
-                    <th>Data de Desativação</th>
+                    <th>Data de Início</th>
+                    <th>Data de Fim</th>
                     <th>Propriedade</th>
                     <th>Tipo de valor</th>
                     <th>Nome do campo no formulário</th>
