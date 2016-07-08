3a37cdaf5b83eafc736dbd88ba3353d5f68d3d56
vmcb <vmcb@vmcb-Aspire-E5-571G>
2016-07-08 16:17:56 +0100

    formularios debugging

--- php/insercao-de-relacoes.php
+++ php/insercao-de-relacoes.php
@@ -236,6 +236,7 @@ class InsereRelacoes
                                             if ($num_props == 0) {
 ?>
                                                 <td colspan="2">Não existem propriedades associadas a este tipo de relação</td>
+                                                <td>
 <?php
                                                 $getRel = $this->bd->runQuery("SELECT * FROM rel_type WHERE id=".$readRelations['rel_type_id']." AND updated_on >'".$readRelations['updated_on']."'");
                                                 if($getRel->num_rows == 0){
@@ -246,6 +247,7 @@ class InsereRelacoes
 ?>
                                                 <a href="insercao-de-relacoes?estado=desativar&rel=<?php echo $readRelations['id'];?>">[Desativar]</a>
                                                 <a href="insercao-de-relacoes?estado=historico&rel=<?php echo $readRelations['id'];?>">[Histórico]</a>
+                                                </td>
 <?php
                                             }
                                             else {
