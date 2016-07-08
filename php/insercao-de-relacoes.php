3165dd946c8fb67b0af4425d3001359e508c3d45
fcl <fabiocl93@gmail.com>
2016-07-08 14:35:55 +0100

    Entidades validação

--- php/insercao-de-relacoes.php
+++ php/insercao-de-relacoes.php
@@ -860,6 +860,7 @@ class InsereRelacoes
                                         {
     ?>
                                         <tr>
+                                              <td><?php echo $verificaRel ?></td>
                                             <td><?php echo $read_SecondEnt['entity_name']; ?></td>
                                             <td> <!--<label>Nome para a relação </label>--><input type="text" name ="nomeDaRel<?php echo $control; ?>"></td>
                                             <td><input type="checkbox" name="secondEnt<?php echo $control; ?>" value="<?php echo $read_SecondEnt['id'];?>"></td>
@@ -871,6 +872,7 @@ class InsereRelacoes
                                             //if the user didn't fave any name to the entity e need to search for the attribute of that entity who has a name.
     ?>
                                         <tr>
+                                             <td><?php echo $verificaRel ?></td>
                                             <td><?php echo  $read_SecondEnt['id']; ?></td>
                                             <td> <!--<label>Nome para a relação </label>--><input type="text" name ="nomeDaRel<?php echo $control; ?>"></td>
                                             <td><input type="checkbox" name="secondEnt<?php echo $control; ?>" value="<?php echo $read_SecondEnt['id'];?>"></td>
@@ -925,7 +927,7 @@ class InsereRelacoes
                                     {
             ?>
                                     <tr>
-                                        <td><?php echo $verificaRel ; /*$read_SecondEnt['entity_name']; */?></td>
+                                        <td><?php echo $read_SecondEnt['entity_name']; ?></td>
                                         <td><input type="text" name ="nomeDaRel<?php echo $control; ?>"></td>
                                         <td><input type="checkbox" name="secondEnt<?php echo $control; ?>" value="<?php echo $read_SecondEnt['id'];?>"></td>
                                     </tr>
@@ -935,7 +937,6 @@ class InsereRelacoes
                                     {               //if the user didn't fave any name to the entity e need to search for the attribute of that entity who has a name.
     ?>
                                         <tr>
-                                            <td><?php echo $verificaRel ?></td>
                                             <td><input type="checkbox" name="secondEnt<?php echo $control; ?>" value="<?php echo $read_SecondEnt['id'];?>"><?php echo  $read_SecondEnt['id']; ?></td>
                                             <td> <!--<label>Nome para a relação </label>--><input type="text" name ="nomeDaRel<?php echo $control; ?>"></td>
                                             <td><input type="checkbox" name="secondEnt<?php echo $control; ?>" value="<?php echo $read_SecondEnt['id'];?>"></td>
